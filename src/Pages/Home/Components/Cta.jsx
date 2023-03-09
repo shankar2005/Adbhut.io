d1c4c24db84943f0831ffe253aadd9c7dc115a6d
Md Maruf Hossain <mickeymaruf@gmail.com>
2023-03-09 12:02:58 +0600

    navigate after select skill from /chat route

--- src/Pages/Home/Components/Cta.jsx
+++ src/Pages/Home/Components/Cta.jsx
@@ -1,14 +1,24 @@
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useRootContext } from '../../../contexts/RootProvider';
 
 const Cta = ({ suggestions, removedSkills, className }) => {
-    const { handleSelectSkill } = useRootContext();
+    const { handleSelectSkill, isMobile } = useRootContext();
+    const pathname = useLocation().pathname;
+    const navigate = useNavigate();
 
     return (
         <div className={`p-2 bg-white ${className || ""}`}>
             <div className='pb-2 skillScroll overflow-x-scroll flex gap-2 text-sm font-medium select-none'>
                 {
                     suggestions.map(skill => <div
-                        onClick={() => handleSelectSkill(skill)}
+                        onClick={() => {
+                            handleSelectSkill(skill)
+                            // this logic is only for if the user is in mobile device
+                            // and select skill from the chat box
+                            if (isMobile && pathname.includes("/projects/chat")) {
+                                navigate("/artists");
+                            }
+                        }}
                         key={`suggestedSkill${skill[1]}`}
                         className='whitespace-nowrap py-1 px-3 border text-blue-500 border-blue-500 rounded-full cursor-pointer hover:bg-blue-100'>
                         {skill[0]}
