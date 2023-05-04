2074c4976587926ba06de66a7886da115ae7d8ce
Md Maruf Hossain <mickeymaruf@gmail.com>
2023-05-05 00:45:06 +0600

    artist profile row view design fixed

--- src/Pages/Artist/Components/View/ArtistRowView.jsx
+++ src/Pages/Artist/Components/View/ArtistRowView.jsx
@@ -1,20 +1,47 @@
+import { useState } from 'react';
 import { FaRegEnvelope } from 'react-icons/fa';
 import { HiPhone } from 'react-icons/hi';
 import { IoLanguageSharp, IoLocationSharp } from 'react-icons/io5';
 import { useRootContext } from '../../../../contexts/RootProvider';
+import brokenImage from "../../../../assets/placeholders/broken.jpg";
 
 const ArtistRowView = ({ artist }) => {
-    const { avatar, setArtistProfile } = useRootContext();
+    const { setArtistProfile } = useRootContext();
+
+    const [imageSrc, setImageSrc] = useState(artist.profile_image);
+    const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+    const handleImageError = () => {
+        setIsImageLoaded(true);
+        setImageSrc(brokenImage);
+    };
+
+    console.log(artist.id);
 
     return (
-        <div className='bg-white border flex gap-3 p-3 rounded-lg'>
-            <div>
-                <img onClick={() => setArtistProfile(artist.id)} className='w-16 h-16 object-cover object-top border p-0.5 bg-white rounded-full cursor-pointer' src={artist.profile_image || avatar} alt="" />
+        <div className='bg-white border-b flex gap-3 p-3'>
+            <div className='relative h-fit'>
+                <img
+                    onClick={() => setArtistProfile(artist.id)}
+                    className='w-[70px] h-[70px] object-cover object-top rounded-full cursor-pointer placeholder:bg-black'
+                    src={imageSrc}
+                    alt="Profile Image"
+                    onError={handleImageError}
+                    onLoad={() => setIsImageLoaded(true)}
+                    style={{ display: isImageLoaded ? 'block' : 'none' }}
+                />
             </div>
             <div>
-                <p onClick={() => setArtistProfile(artist.id)} className='font-medium cursor-pointer'>{artist.name}</p>
-                {artist.artist_intro && <p className='text-xs mb-2'>{artist.artist_intro}</p>}
-                <div className='text-xs mb-2'>
+                <p onClick={() => setArtistProfile(artist.id)} className='font-medium cursor-pointer'>
+                    {artist.name}
+                </p>
+                <div className='text-xs flex flex-wrap gap-2'>
+                    {
+                        artist?.skill?.map((skill, idx) => <div key={idx} className='bg-sky-400 p-1 rounded-full text-white'>{skill}</div>)
+                    }
+                </div>
+                {artist.artist_intro && <div className='w-fit my-1 px-0.5 text-sm font-normal font-sans italic text-gray-700 bg-yellow-100'>&#9679; {artist.artist_intro}</div>}
+                <div className='text-xs'>
                     {artist.email && <p className='flex items-center gap-2'><FaRegEnvelope />{artist.email}</p>}
                     {artist.phone && <p className='flex items-center gap-2'><HiPhone />{artist.phone}</p>}
                     {
@@ -22,11 +49,6 @@ const ArtistRowView = ({ artist }) => {
                         <p className='flex items-center gap-2'><IoLanguageSharp /> {artist.languages.join(", ")}</p>
                     }
                     <p className='flex items-center gap-2'><IoLocationSharp /> {artist.location}</p>
-                </div>
-                <div className='text-xs flex flex-wrap gap-2'>
-                    {
-                        artist?.skills?.map((skill, idx) => <div key={idx} className='bg-sky-400 p-1 rounded text-white'>{skill}</div>)
-                    }
                 </div>
             </div>
         </div>
