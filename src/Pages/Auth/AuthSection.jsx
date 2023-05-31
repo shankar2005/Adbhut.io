baa804f11cc3129fbea3d9a3e6bcb8162b1e860d
Md Maruf Hossain <mickeymaruf@gmail.com>
2023-05-31 19:04:14 +0600

    login form validation added

--- src/Pages/Auth/AuthSection.jsx
+++ src/Pages/Auth/AuthSection.jsx
@@ -20,7 +20,7 @@ const AuthSection = () => {
         <div className="pt-6 text-black bg-white w-96 max-h-[90vh] border rounded-md p-5 shadow-2xl overflow-y-auto scroll-none relative">
             <button onClick={closeModal} className='absolute right-3 top-3'><RxCross1 size={25} /></button>
 
-            <img className='w-16 mx-auto mb-5' src={logo} alt="" />
+            <img className='w-16 mx-auto mb-3' src={logo} alt="" />
             {
                 isLoginForm
                     ? <h2 className="mb-3 text-2xl font-semibold text-center">Login to your account</h2>
@@ -43,12 +43,12 @@ const AuthSection = () => {
                     </p>
             }
 
-            <div className="flex items-center w-full my-2">
+            <div className="hidden flex items-center w-full my-2">
                 <hr className="w-full text-gray-400" />
                 <p className="px-3 text-gray-400">OR</p>
                 <hr className="w-full text-gray-400" />
             </div>
-            <div className="flex items-center justify-center gap-2 my-3 text-gray-600">
+            <div className="invisible flex items-center justify-center gap-2 my-3 text-gray-600">
                 <button className=''><AiOutlineMail className='w-6 h-6' /></button>
                 <button className=''><AiOutlineGoogle className='w-7 h-7' /></button>
                 <button className=''><AiFillLinkedin className='w-6 h-6' /></button>
--- src/Pages/Auth/LoginForm.jsx
+++ src/Pages/Auth/LoginForm.jsx
@@ -6,18 +6,22 @@ import Cookies from 'universal-cookie';
 import { useLoginUserMutation, useVerifyUserMutation } from '../../features/auth/authApi';
 import { setLoading, setToken, setUser } from '../../features/auth/authSlice';
 import { closeLogin } from '../../features/dropdown/dropdownSlice';
+import { BiHide, BiShowAlt } from "react-icons/bi";
+import Badge from '../../Components/Badge/Badge';
+import { useState } from 'react';
 
 const LoginForm = () => {
     const [loginUser, { data, isError, error, isLoading }] = useLoginUserMutation();
     const [verifyUser, { data: userData }] = useVerifyUserMutation();
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const [showPassword, setShowPassword] = useState(false);
 
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = data => {
         dispatch(setLoading(true));
         loginUser({
-            email: data.email,
+            email: data.email.trim(),
             password: data.password
         });
     };
@@ -46,14 +50,32 @@ const LoginForm = () => {
             <div className="space-y-4 mb-4">
                 <div className="space-y-2">
                     <label htmlFor="email" className="block text-sm">Email</label>
-                    <input type="text" {...register("email", { required: true })} id="email" placeholder="Enter email" className="w-full p-3 border rounded-md border-gray-700" data-temp-mail-org="2" />
+                    <input
+                        type="text"
+                        {...register("email", {
+                            required: true,
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: "Provide a valid email address"
+                            }
+                        })}
+                        id="email"
+                        placeholder="Enter email"
+                        className={`input !py-3 rounded ${errors.email?.message && "!border-red-500 !ring-red-500"}`}
+                    />
+                    {errors.email?.message && <small className='text-red-400'>{errors.email?.message}</small>}
                 </div>
-                <div className="space-y-2">
+                <div className="space-y-2 relative">
                     <div className="flex justify-between">
                         <label htmlFor="password" className="text-sm">Password</label>
                         <a rel="noopener noreferrer" href="#" className="text-xs hover:underline text-gray-400">Forgot password?</a>
                     </div>
-                    <input type="password" {...register("password", { required: true })} id="password" placeholder="*****" className="w-full p-3 border rounded-md border-gray-700" />
+                    <input type={showPassword ? "text" : "password"} {...register("password", { required: true })} id="password" placeholder="*****" className="input !py-3 rounded" />
+                    <div onClick={() => setShowPassword(prev => !prev)} className="absolute bottom-0 right-0 -translate-x-2.5 -translate-y-2.5 text-gray-400 cursor-pointer">
+                        <Badge type="success" className="block text-sm">
+                            {showPassword ? <BiHide size={20} /> : <BiShowAlt size={20} />}
+                        </Badge>
+                    </div>
                 </div>
             </div>
             {
@@ -61,9 +83,7 @@ const LoginForm = () => {
                     ? <button type="submit" className="w-full px-8 py-3 font-medium rounded-md bg-gray-300 text-gray-400 animate-pulse" disabled>Loading...</button>
                     : <button type="submit" className="w-full px-8 py-3 font-medium rounded-md bg-blue-500 hover:bg-blue-600 text-white">Sign in</button>
             }
-            {
-                isError && <p className='text-red-500 text-sm mt-3'>{error.message}</p>
-            }
+            {isError && <p className='bg-red-100 text-red-500 text-sm p-3 mt-3'>{error?.data?.error || "server_error::SOMETHING_WENT_WRONG"}</p>}
         </form>
     );
 };
