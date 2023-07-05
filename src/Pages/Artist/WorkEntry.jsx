ad3279f08eb94b9ded502c7177093119ef1107a6
Md Maruf Hossain <mickeymaruf@gmail.com>
2023-07-05 16:30:41 +0600

    added an icon in workEntry instead done button

--- src/Pages/Artist/WorkEntry.jsx
+++ src/Pages/Artist/WorkEntry.jsx
@@ -1,3 +1,4 @@
+import { BsArrowRightCircle } from 'react-icons/bs';
 import { Link, useParams } from 'react-router-dom';
 import Button from '../../Components/Button/Button';
 import Container from '../../Components/Container/Container';
@@ -10,7 +11,9 @@ const WorkEntry = () => {
         <Container className="p-4 flex flex-col">
             <h4 className='my-4 font-hero font-semibold text-lg text-blue-600/80'>Add work link</h4>
             <WorkLinkTable artistId={artistId} />
-            <Link to={`/artists/edit-artist/${artistId}`} className="block ml-auto mt-5"><Button type="button" variant="secondary">Done</Button></Link>
+            <Link to={`/artists/edit-artist/${artistId}`} className="block ml-auto mt-5 text-gray-500">
+                <BsArrowRightCircle size={30} />
+            </Link>
         </Container>
     );
 };
